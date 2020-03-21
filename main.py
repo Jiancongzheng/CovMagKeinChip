abf9b272f50399cf52169b0436fd868ff20c2ffc
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-03-21 17:42:49 +0100

    add main function

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from service.crawler import Crawler
+
+
+# In[2]:
+
+
+crawler = Crawler()
+info = crawler.get_response()
+
+
+# In[3]:
+
+
+info
+
